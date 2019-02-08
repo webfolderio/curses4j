407eeb1b93cbd57c1b5c7ce6e74807346cdd56cb
webfolderio <support@webfolder.io>
2019-02-08 15:39:52 +0300

    Deleted travis

--- build-macos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcc -fPIC -Wall -shared -I$JAVA_HOME/include -I$JAVA_HOME/include/linux src/main/native/curses4j.c -o libcurses4j.dylib -lncurses -ltinfo
-strip libcurses4j.dylib
-cp libcurses4j.dylib src/main/resources/META-INF
