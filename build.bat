0656dbbd7c06c7f76bf315719231322b5f04174c
webfolderio <support@webfolder.io>
2019-02-17 14:53:41 +0300

    Update PDCurses to 3.8

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
-"%JAVA_HOME%\bin\jar.exe" xf 3.7.zip
-copy /Y src\main\native\libobjs.mif PDCurses-3.7\common\libobjs.mif
-copy /Y src\main\native\Makefile.vc PDCurses-3.7\wincon\Makefile.vc
-copy /Y src\main\native\curses4j.c PDCurses-3.7\pdcurses\curses4j.c
-cd PDCurses-3.7\wincon
+"%JAVA_HOME%\bin\jar.exe" xf 3.8.zip
+copy /Y src\main\native\libobjs.mif PDCurses-3.8\common\libobjs.mif
+copy /Y src\main\native\Makefile.vc PDCurses-3.8\wincon\Makefile.vc
+copy /Y src\main\native\curses4j.c PDCurses-3.8\pdcurses\curses4j.c
+cd PDCurses-3.8\wincon
 nmake -f Makefile.vc UTF8=Y WIDE=Y DLL=Y clean all
 cd ..
 cd ..
-copy /Y PDCurses-3.7\wincon\pdcurses.dll src\main\resources\META-INF\curses4j.dll
+copy /Y PDCurses-3.8\wincon\pdcurses.dll src\main\resources\META-INF\curses4j.dll
